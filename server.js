931b7b9c3ff4510000f4aa063b0746ece383bfd9
gregory.desvaux <gregory.desvaux@gmail.com>
2016-10-15 18:23:56 +0200

    Url parameters changed

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 var http = require("http"),
     url = require("url"),
     spawn = require('child_process').spawn,
-    port = (process.argv[2] || 8888);
+    port = (process.argv[2] || 3000);
 
 http.createServer(function(request, response) {
-  var q = url.parse(request.url, true).query;
-  var uri = q.url;
-  var action = q.action;
-  if (!action) {
-     action = "render";
+  var uri  = url.parse(request.url, true), action = "render";
+  
+  if (uri.pathname.startsWith("/http")) {
+    uri =  uri.pathname.substring(1);
+  } else {
+    if (uri.query.action)
+       action = uri.query.action;
+
+    uri = uri.query.url;
   }
 
   if (uri) {
     //user told us their uri in the GET request, ex: http://localhost:8888/?url=http%3A%2F%2Fwww.example.com 
-    console.log('Request at ' + new Date());
-    console.log(action + ' ' + uri);
+    console.log(new Date() + ' - GET - ' + uri);
     response.headers = {
      'Cache': 'no-cache',
      'Content-Type': 'text/html'
@@ -31,7 +34,7 @@ http.createServer(function(request, response) {
 
     phantom.on('exit', function (code) {
       response.end();
-      console.log('child process exited with code ' + code);
+      //console.log(uri + ' finished ' + code);
     });
   } else {
      response.statusCode = 403;
